b57ec7996a2967fb95e69d2a56f59ed7149f7950
chakib <chakibdaii1995@gmail.com>
2024-12-22 21:14:48 +0100

    Naftah - added main class

---
+++ src/main/java/org/daiitech/naftah/Naftah.java
@@ -0,0 +1,13 @@
+package org.daiitech.naftah;
+
+/**
+ * @author Chakib
+ * @created 22/12/2024
+ * @project naftah
+ **/
+public class Naftah {
+
+    public static void main(String[] args) {
+    }
+
+}
