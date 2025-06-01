39fca959ebf47eb112ac01682452a4e59d37c8f3
chakib <chakibdaii1995@gmail.com>
2025-06-01 17:17:10 +0200

    Naftah - added DefaultNaftahParserVisitor as concrete implementation of the NaftahParserVisitor

--- src/main/java/org/daiitech/naftah/core/parser/DefaultNaftahParserVisitor.java
+++ src/main/java/org/daiitech/naftah/core/parser/DefaultNaftahParserVisitor.java
@@ -5,4 +5,5 @@ package org.daiitech.naftah.core.parser;
  * @author Chakib Daii
  **/
 public class DefaultNaftahParserVisitor extends org.daiitech.naftah.core.parser.NaftahParserBaseVisitor<Object> {
+
 }
