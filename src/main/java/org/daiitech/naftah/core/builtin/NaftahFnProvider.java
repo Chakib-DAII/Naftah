1e3412b951a60d1c6ba49801b8b09e1c2aa45c60
chakib <chakibdaii1995@gmail.com>
2025-06-01 04:08:35 +0200

    Naftah - updated NaftahFnProvider

--- src/main/java/org/daiitech/naftah/core/builtin/NaftahFnProvider.java
+++ src/main/java/org/daiitech/naftah/core/builtin/NaftahFnProvider.java
@@ -12,7 +12,7 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
  *
  * @author Chakib Daii
  **/
-@Target(ElementType.METHOD)
+@Target(ElementType.TYPE)
 @Retention(RUNTIME)
 public @interface NaftahFnProvider {
     String name();
