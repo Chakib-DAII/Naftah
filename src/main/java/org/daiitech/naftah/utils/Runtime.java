d030c21df3956b7880a715f678bcac0208edc64c
chakib <chakibdaii1995@gmail.com>
2025-06-06 22:29:54 +0200

    Naftah - minor changes

--- src/main/java/org/daiitech/naftah/utils/Runtime.java
+++ src/main/java/org/daiitech/naftah/utils/Runtime.java
@@ -2,5 +2,8 @@ package org.daiitech.naftah.utils;
 
 /**
  * @author Chakib Daii
+ *
+ * TODO: add runtiome ralted things
+ * TODO: calling functions, retriving vars, passing them....
  */
 public interface Runtime {}
