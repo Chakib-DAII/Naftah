1fc2dae2ac509c7d719718c93e50a710551d07f2
chakib <chakibdaii1995@gmail.com>
2025-06-07 18:02:52 +0200

    Naftah - updated NaftahParserHelper

--- src/main/java/org/daiitech/naftah/core/parser/NaftahParserHelper.java
+++ src/main/java/org/daiitech/naftah/core/parser/NaftahParserHelper.java
@@ -1,7 +1,14 @@
 package org.daiitech.naftah.core.parser;
 
+import org.antlr.v4.runtime.misc.Pair;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.Tree;
+import org.daiitech.naftah.core.builtin.lang.DeclaredParameter;
+import org.daiitech.naftah.core.builtin.lang.DeclaredFunction;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * @author Chakib Daii
@@ -14,6 +21,105 @@ public class NaftahParserHelper {
 
     public static  Object visit(org.daiitech.naftah.core.parser.NaftahParserBaseVisitor naftahParserBaseVisitor, ParseTree tree) {
         return naftahParserBaseVisitor.visit(tree);
+    }
+
+    public static void prepareDeclaredFunction(org.daiitech.naftah.core.parser.NaftahParserBaseVisitor naftahParserBaseVisitor,
+                                               DeclaredFunction function) {
+        if (function.getParameters() == null && hasChild(function.getParametersContext()))
+            function.setParameters((List<DeclaredParameter>) visit(naftahParserBaseVisitor, function.getParametersContext()));
+        if (function.getReturnType() == null && hasChild(function.getReturnTypeContext()))
+            function.setReturnType(visit(naftahParserBaseVisitor, function.getReturnTypeContext()));
+    }
+
+    public static Map<String, Object> prepareDeclaredFunctionArguments(org.daiitech.naftah.core.parser.NaftahParserBaseVisitor naftahParserBaseVisitor,
+                                                                       List<DeclaredParameter> parameters, List<Pair<String, Object>> arguments) {
+        if (parameters.size() < arguments.size())
+            throw new RuntimeException("Too many arguments");
+
+        // how many params don't have defaults
+        List<DeclaredParameter> requiredParams = parameters.stream()
+                .filter(p -> p.getDefaultValue() == null)
+                .toList();
+
+        // arguments that have names
+        Map<Integer, Pair<String, Object>> namedArguments = IntStream.range(0, arguments.size())
+                .mapToObj(i -> {
+                    var current = arguments.get(i);
+                    return current.a != null ? Map.entry(i, current) : null;
+                })
+                .filter(Objects::nonNull)
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+        Map<String, Object> finalArguments = null;
+        if (namedArguments.isEmpty()) {
+            if (arguments.size() < requiredParams.size()) throw new RuntimeException("Too few arguments");
+            if (requiredParams.size() < arguments.size())
+                throw new RuntimeException("Too many unnamed arguments");
+            // process non named args
+            finalArguments =
+                    IntStream.range(0, arguments.size())
+                            .mapToObj(i -> {
+                                var argument = arguments.get(i);
+                                var param = requiredParams.get(i);
+                                return Map.entry(param.getName(), argument.b);
+                            })
+                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        } else {
+            Set<String> usedNames = new HashSet<>();
+            finalArguments = new HashMap<>();
+            // arguments that have no names
+            Map<Integer, Pair<String, Object>> positionalArguments =
+                    IntStream.range(0, arguments.size())
+                            .mapToObj(i -> {
+                                var current = arguments.get(i);
+                                return current.a == null ? Map.entry(i, current) : null;
+                            })
+                            .filter(Objects::nonNull)
+                            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+
+            // Assign positional arguments
+            for (var entry : positionalArguments.entrySet()) {
+                String paramName = parameters.get(entry.getKey()).getName();
+                if (namedArguments.containsKey(entry.getKey())) {
+                    throw new RuntimeException("Argument '" + paramName + "' specified both positionally and by name");
+                }
+                finalArguments.put(paramName, entry.getValue().b);
+                usedNames.add(paramName);
+            }
+
+            // Assign named arguments
+            for (var entry : namedArguments.entrySet()) {
+                DeclaredParameter param = parameters.get(entry.getKey());
+
+                if (param != null) {
+                    String paramName = param.getName();
+
+                    if (usedNames.contains(paramName)) {
+                        throw new RuntimeException("Duplicate argument: " + paramName);
+                    }
+
+                    finalArguments.put(paramName, entry.getValue().b);
+                    usedNames.add(paramName);
+
+                } else {
+                    throw new RuntimeException("Unknown parameter name: " + entry.getValue().a);
+                }
+            }
+
+            // Assign default values
+            for (DeclaredParameter param : parameters) {
+                if (!finalArguments.containsKey(param.getName())) {
+                    if (param.getDefaultValue() != null) {
+                        finalArguments.put(param.getName(), param.getDefaultValue());
+                    } else {
+                        throw new RuntimeException("Missing required argument: " + param.getName());
+                    }
+                }
+            }
+        }
+
+        return finalArguments;
     }
 
 }
