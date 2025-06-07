c7a1f4917fa42ad3384f46391f76d11dc9aa5d4e
chakib <chakibdaii1995@gmail.com>
2025-06-07 21:34:39 +0200

    Naftah - updated ObjectUtils

--- src/main/java/org/daiitech/naftah/core/builtin/utils/ObjectUtils.java
+++ src/main/java/org/daiitech/naftah/core/builtin/utils/ObjectUtils.java
@@ -3,6 +3,7 @@ package org.daiitech.naftah.core.builtin.utils;
 import java.lang.reflect.Array;
 import java.util.Collection;
 import java.util.Map;
+import java.util.Optional;
 
 /**
  * @author Chakib Daii
@@ -45,5 +46,45 @@ public final class ObjectUtils {
             // logical negation
             return !isTruthy(value);
         }
+    }
+
+    /**
+     * Determine whether the given object is empty.
+     * <p>This method supports the following object types.
+     * <ul>
+     * <li>{@code Optional}: considered empty if not {@link Optional#isPresent()}</li>
+     * <li>{@code Array}: considered empty if its length is zero</li>
+     * <li>{@link CharSequence}: considered empty if its length is zero</li>
+     * <li>{@link Collection}: delegates to {@link Collection#isEmpty()}</li>
+     * <li>{@link Map}: delegates to {@link Map#isEmpty()}</li>
+     * </ul>
+     * <p>If the given object is non-null and not one of the aforementioned
+     * supported types, this method returns {@code false}.
+     * @param obj the object to check
+     * @return {@code true} if the object is {@code null} or <em>empty</em>
+     */
+    public static boolean isEmpty(Object obj) {
+        if (obj == null) {
+            return true;
+        }
+
+        if (obj instanceof Optional<?> optional) {
+            return optional.isEmpty();
+        }
+        if (obj instanceof CharSequence charSequence) {
+            return charSequence.isEmpty();
+        }
+        if (obj.getClass().isArray()) {
+            return Array.getLength(obj) == 0;
+        }
+        if (obj instanceof Collection<?> collection) {
+            return collection.isEmpty();
+        }
+        if (obj instanceof Map<?, ?> map) {
+            return map.isEmpty();
+        }
+
+        // else
+        return false;
     }
 }
