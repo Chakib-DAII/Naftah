40ad98e102b37bd97a9b3e67c45aa2c0443dae45
chakib <chakibdaii1995@gmail.com>
2025-06-19 01:12:05 +0200

    Naftah - updated Naftah main as a commandline itself

--- src/main/java/org/daiitech/naftah/Naftah.java
+++ src/main/java/org/daiitech/naftah/Naftah.java
@@ -1,16 +1,20 @@
 package org.daiitech.naftah;
 
-import static org.daiitech.naftah.NaftahSystem.setupOutput;
+import static java.util.logging.Logger.*;
+import static org.daiitech.naftah.NaftahSystem.setupErrorStream;
+import static org.daiitech.naftah.NaftahSystem.setupOutputStream;
 import static org.daiitech.naftah.builtin.utils.ObjectUtils.isSimpleOrCollectionOrMapOfSimpleType;
 import static org.daiitech.naftah.parser.DefaultContext.bootstrap;
+import static org.daiitech.naftah.utils.reflect.RuntimeClassScanner.CLASS_PATH_PROPERTY;
+import static picocli.CommandLine.*;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.PrintStream;
 import java.io.PrintWriter;
-import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
+import java.util.logging.*;
+
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
@@ -26,6 +30,9 @@ import picocli.CommandLine;
  *     <p>main of Naftah programming language as an interpreted JVM language
  */
 public final class Naftah {
+
+  public static final String SCAN_CLASSPATH_PROPERTY = "scanClassPath";
+  public static final String FILE_ENCODING_PROPERTY = "file.encoding";
 
   private static final String[] STANDARD_EXTENSIONS = {".naftah", ".nfth", ".na", ".nsh"};
 
@@ -44,35 +51,10 @@ public final class Naftah {
   private Naftah() {}
 
   public static void main(String[] args) throws IOException {
-    bootstrap();
-    initLogger();
-    setupOutput();
-    // TODO: update this logic to be more sophisticated and handle args and commands
-    // Create an input stream from the Naftah code
-    CharStream input =
-        CharStreams.fromPath(searchForNaftahScriptFile(args[0]).toPath(), StandardCharsets.UTF_8);
-
-    // Create a lexer and token stream
-    NaftahLexer lexer = new NaftahLexer(input);
-    CommonTokenStream tokens = new CommonTokenStream(lexer);
-
-    // Create a parser
-    NaftahParser parser = new NaftahParser(tokens);
-
-    // Parse the input and get the parse tree
-    ParseTree tree = parser.program();
-
-    // Create a visitor and visit the parse tree
-    DefaultNaftahParserVisitor visitor = new DefaultNaftahParserVisitor();
-    var result = visitor.visit(tree);
-
-    if (isSimpleOrCollectionOrMapOfSimpleType(result)) System.out.println(result);
-
-    System.out.println();
-    System.exit(0);
+    processArgs(args);
   }
 
-  private static void initLogger() {
+  private static void initLogger(boolean debug) {
     try {
       String loggingConfigFile = System.getProperty("java.util.logging.config.file");
 
@@ -86,6 +68,22 @@ public final class Naftah {
       } catch (IOException ex) {
         throw new RuntimeException(ex);
       }
+    } finally {
+     if (debug) {
+       // Adjust ConsoleHandler (if it exists)
+       Logger rootLogger = getLogger("");
+       for (Handler h : rootLogger.getHandlers()) {
+         if (h instanceof ConsoleHandler consoleHandler) {
+           consoleHandler.setLevel(Level.FINEST);
+         } else if (h instanceof FileHandler fileHandler) {
+           fileHandler.setLevel(Level.FINE);
+         }
+       }
+
+       // Adjust individual loggers
+       Logger.getLogger("DefaultNaftahParserVisitor").setLevel(Level.FINEST);
+       Logger.getLogger("org.daiitech.naftah").setLevel(Level.FINE);
+     }
     }
   }
 
@@ -111,7 +109,17 @@ public final class Naftah {
     return scriptFile;
   }
 
-  public static class VersionProvider implements CommandLine.IVersionProvider {
+  public static CharStream getCharStream(boolean isScriptFile, String script) throws IOException {
+    CharStream charStream;
+    if (isScriptFile) {
+      charStream = CharStreams.fromPath(searchForNaftahScriptFile(script).toPath(), StandardCharsets.UTF_8);
+    } else {
+      charStream =  CharStreams.fromString(script);
+    }
+    return charStream;
+  }
+
+  public static class VersionProvider implements IVersionProvider {
     @Override
     public String[] getVersion() {
       return new String[] {
@@ -127,7 +135,7 @@ public final class Naftah {
     }
   }
 
-  @CommandLine.Command(
+  @Command(
       name = "naftah",
       customSynopsis = "naftah [options] [filename] [args]",
       description = "The Naftah command line processor.",
@@ -140,133 +148,80 @@ public final class Naftah {
     // can extract it and the JVM can be started with the classpath already correctly set.
     // This saves us from having to fork a new JVM process with the classpath set from the processed
     // arguments.
-    @CommandLine.Option(
+    @Option(
         names = {"-cp", "-classpath", "--classpath"},
         paramLabel = "<path>",
         description = "Specify where to find the class files - must be first argument")
     private String classpath;
 
-    @CommandLine.Option(
+    @Option(
         names = {"-D", "--define"},
         paramLabel = "<property=value>",
         description = "Define a system property")
     private Map<String, String> systemProperties = new LinkedHashMap<>();
 
-    @CommandLine.Option(
-        names = "--disableopt",
-        paramLabel = "optlist",
-        split = ",",
-        description = {
-          "Disables one or all optimization elements; optlist can be a comma separated list with the elements: ",
-          "all (disables all optimizations), ",
-          "int (disable any int based optimizations)"
-        })
-    private List<String> disableopt = new ArrayList<String>();
-
-    @CommandLine.Option(
+    @Option(
         names = {"-d", "--debug"},
         description = "Debug mode will print out full stack traces")
     private boolean debug;
 
-    @CommandLine.Option(
+    @Option(
         names = {"-c", "--encoding"},
         paramLabel = "<charset>",
         description = "Specify the encoding of the files")
     private String encoding;
 
-    @CommandLine.Option(
+    @Option(
+            names = {"-scp", "--scan-classpath"},
+            paramLabel = "<charset>",
+            description = "Specify if the classpath classes should be reused as nafta types")
+    private boolean scanClasspath;
+
+
+    @Option(
         names = {"-e"},
         paramLabel = "<script>",
         description = "Specify a command line script")
     private String script;
 
-    @CommandLine.Option(
-        names = {"-i"},
-        arity = "0..1",
-        paramLabel = "<extension>",
-        description = "Modify files in place; create backup if extension is given (e.g. '.bak')")
-    private String extension;
-
-    @CommandLine.Option(
-        names = {"-n"},
-        description = "Process files line by line using implicit 'line' variable")
-    private boolean lineByLine;
-
-    @CommandLine.Option(
-        names = {"-p"},
-        description = "Process files line by line and print result (see also -n)")
-    private boolean lineByLinePrint;
-
-    @CommandLine.Option(
-        names = {"-pa", "--parameters"},
-        description = "Generate metadata for reflection on method parameter names (jdk8+ only)")
-    private boolean parameterMetadata;
-
-    @CommandLine.Option(
-        names = {"-pr", "--enable-preview"},
-        description = "Enable preview Java features (jdk12+ only)")
-    private boolean previewFeatures;
-
-    @CommandLine.Option(
-        names = "-l",
-        arity = "0..1",
-        paramLabel = "<port>",
-        description = "Listen on a port and process inbound lines (default: 1960)")
-    private String port;
-
-    @CommandLine.Option(
-        names = {"-a", "--autosplit"},
-        arity = "0..1",
-        paramLabel = "<splitPattern>",
-        description =
-            "Split lines using splitPattern (default '\\s') using implicit 'split' variable")
-    private String splitPattern;
-
-    @CommandLine.Option(
-        names = {"--configscript"},
-        paramLabel = "<script>",
-        description = "A script for tweaking the configuration options")
-    private String configscript;
-
-    @CommandLine.Option(
-        names = {"-b", "--basescript"},
-        paramLabel = "<class>",
-        description = "Base class name for scripts (must derive from Script)")
-    private String scriptBaseClass;
-
-    @CommandLine.Option(
+    @Option(
         names = {"-h", "--help"},
         usageHelp = true,
         description = "Show this help message and exit")
     private boolean helpRequested;
 
-    @CommandLine.Option(
+    @Option(
         names = {"-v", "--version"},
         versionHelp = true,
         description = "Print version information and exit")
     private boolean versionRequested;
 
-    @CommandLine.Option(
-        names = {"--compile-static"},
-        description = "Use CompileStatic")
-    private boolean compileStatic;
-
-    @CommandLine.Option(
-        names = {"--type-checked"},
-        description = "Use TypeChecked")
-    private boolean typeChecked;
-
-    @CommandLine.Unmatched List<String> arguments = new ArrayList<>();
+    @Unmatched List<String> arguments = new ArrayList<>();
 
     /**
      * Process the users request.
      *
      * @param parser the parsed command line. Used when the user input was invalid.
-     * @throws CommandLine.ParameterException if the user input was invalid
+     * @throws ParameterException if the user input was invalid
      */
-    boolean process(CommandLine parser) throws CommandLine.ParameterException, IOException {
+    boolean process(CommandLine parser) throws ParameterException, IOException {
+      // append to classpath
+      if (Objects.nonNull(classpath)) {
+        final String actualClasspath = System.getProperty(CLASS_PATH_PROPERTY);
+        System.setProperty(CLASS_PATH_PROPERTY, actualClasspath + File.pathSeparator + classpath);
+      }
+
+      // append system properties
       for (Map.Entry<String, String> entry : systemProperties.entrySet()) {
         System.setProperty(entry.getKey(), entry.getValue());
+      }
+
+      if(Objects.nonNull(encoding)) {
+      System.setProperty(FILE_ENCODING_PROPERTY, encoding);
+      }
+
+      if(scanClasspath) {
+        System.setProperty(SCAN_CLASSPATH_PROPERTY, Boolean.toString(true));
       }
 
       final Naftah main = new Naftah();
@@ -276,14 +231,10 @@ public final class Naftah {
       main.isScriptFile = script == null;
       if (main.isScriptFile) {
         if (arguments.isEmpty()) {
-          throw new CommandLine.ParameterException(
+          throw new ParameterException(
               parser, "error: neither -e or filename provided");
         }
         main.script = arguments.remove(0);
-        if (main.script.endsWith(".java")) {
-          throw new CommandLine.ParameterException(
-              parser, "error: cannot compile file with .java extension: " + main.script);
-        }
       } else {
         main.script = script;
       }
@@ -294,40 +245,41 @@ public final class Naftah {
     }
   }
 
-  static void processArgs(String[] args, final PrintStream out, final PrintStream err) {
+  static void processArgs(String[] args) {
+    setupOutputStream();
+    setupErrorStream();
     NaftahCommand naftahCommand = new NaftahCommand();
 
     CommandLine parser =
         new CommandLine(naftahCommand)
-            .setOut(new PrintWriter(out))
-            .setErr(new PrintWriter(err))
+            .setOut(new PrintWriter(System.out))
+            .setErr(new PrintWriter(System.err))
             .setUnmatchedArgumentsAllowed(true)
             .setStopAtUnmatched(true);
 
     try {
-      CommandLine.ParseResult result = parser.parseArgs(args);
+      ParseResult result = parser.parseArgs(args);
 
-      if (CommandLine.printHelpIfRequested(result)) {
+      if (printHelpIfRequested(result)) {
         return;
       }
 
-      // TODO: pass printstream(s) down through process
       if (!naftahCommand.process(parser)) {
         // If we fail, then exit with an error so scripting frameworks can catch it.
         System.exit(1);
       }
 
-    } catch (CommandLine.ParameterException ex) { // command line arguments could not be parsed
-      err.println(ex.getMessage());
-      ex.getCommandLine().usage(err);
+    } catch (ParameterException ex) { // command line arguments could not be parsed
+      System.err.println(ex.getMessage());
+      ex.getCommandLine().usage(System.err);
     } catch (IOException ioe) {
-      err.println("error: " + ioe.getMessage());
+      System.err.println("error: " + ioe.getMessage());
     }
   }
 
   private boolean run() {
     try {
-      processOnce();
+      runLang();
       return true;
     } catch (Throwable e) {
       System.err.println("Caught: " + e);
@@ -338,10 +290,31 @@ public final class Naftah {
     }
   }
 
-  private void processOnce() throws IOException, URISyntaxException {
-    //        NaftahShell naftah = new NaftahShell(Thread.currentThread().getContextClassLoader(),
-    // conf);
-    //        setupContextClassLoader(naftah);
-    //        naftah.run(getScriptSource(isScriptFile, script), args);
+  private void runLang() throws IOException {
+    bootstrap();
+    initLogger(debug);
+    // TODO: update this logic to be more sophisticated and handle args and commands
+    // Create an input stream from the Naftah code
+    CharStream input = getCharStream(isScriptFile, script);
+
+
+    // Create a lexer and token stream
+    NaftahLexer lexer = new NaftahLexer(input);
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+    // Create a parser
+    NaftahParser parser = new NaftahParser(tokens);
+
+    // Parse the input and get the parse tree
+    ParseTree tree = parser.program();
+
+    // Create a visitor and visit the parse tree
+    DefaultNaftahParserVisitor visitor = new DefaultNaftahParserVisitor();
+    var result = visitor.visit(tree);
+
+    if (isSimpleOrCollectionOrMapOfSimpleType(result)) System.out.println(result);
+
+    System.out.println();
+    System.exit(0);
   }
 }
