6f2849b86dcb218a8936ab0896a72d0a6f3004c9
chakib <chakibdaii1995@gmail.com>
2025-06-20 19:42:07 +0200

    Naftah - updated formatter

--- src/main/java/org/daiitech/naftah/utils/arabic/ArabicLogFormatter.java
+++ src/main/java/org/daiitech/naftah/utils/arabic/ArabicLogFormatter.java
@@ -1,5 +1,6 @@
 package org.daiitech.naftah.utils.arabic;
 
+import static org.daiitech.naftah.utils.arabic.ArabicUtils.containsArabic;
 import static org.daiitech.naftah.utils.arabic.ArabicUtils.shape;
 
 import java.util.logging.Formatter;
@@ -17,11 +18,14 @@ public class ArabicLogFormatter extends Formatter {
   public String format(LogRecord record) {
     String msg = record.getMessage();
 
-    try {
-      String visual = shape(msg);
-      return LOG_MSG.formatted(record.getLevel(), visual);
-    } catch (Exception e) {
+    if (containsArabic(msg)) {
+      try {
+        String visual = shape(msg);
+        return LOG_MSG.formatted(record.getLevel(), visual);
+      } catch (Exception e) {
+        return LOG_MSG.formatted(record.getLevel(), msg); // fallback
+      }
+    } else
       return LOG_MSG.formatted(record.getLevel(), msg); // fallback
-    }
   }
 }
