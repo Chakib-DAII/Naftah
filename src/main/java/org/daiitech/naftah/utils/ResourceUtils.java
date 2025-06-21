3e5c3820e02b5339ab27d980e621a677b80cb94c
chakib <chakibdaii1995@gmail.com>
2025-06-21 14:41:45 +0200

    Naftah - updated classpath caching logic - ResourceUtils

--- src/main/java/org/daiitech/naftah/utils/ResourceUtils.java
+++ src/main/java/org/daiitech/naftah/utils/ResourceUtils.java
@@ -1,8 +1,11 @@
-package org.daiitech.naftah.builtin.utils;
+package org.daiitech.naftah.utils;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -30,5 +33,18 @@ public final class ResourceUtils {
     } catch (URISyntaxException e) {
       throw new RuntimeException("Could not determine JAR directory", e);
     }
+  }
+
+  /**
+   * Opens an {@link InputStream} reading from the given URL with/without caching the stream. This
+   * prevents file descriptor leaks when reading from file system URLs.
+   *
+   * @param url the URL to connect to
+   * @return an input stream reading from the URL connection
+   */
+  public static InputStream openStream(URL url, boolean useCaches) throws IOException {
+    URLConnection urlConnection = url.openConnection();
+    urlConnection.setUseCaches(useCaches);
+    return urlConnection.getInputStream();
   }
 }
