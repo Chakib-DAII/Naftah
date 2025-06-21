9957ab7ec574599e926c3df9878a2dff0bbc67ca
chakib <chakibdaii1995@gmail.com>
2025-06-21 21:02:07 +0200

    Naftah - updated highlighters logic

--- src/main/java/org/daiitech/naftah/utils/jline/BaseHighlighter.java
+++ src/main/java/org/daiitech/naftah/utils/jline/BaseHighlighter.java
@@ -1,9 +1,12 @@
 package org.daiitech.naftah.utils.jline;
 
+import java.util.Objects;
 import java.util.regex.Pattern;
 import org.jline.reader.Highlighter;
 import org.jline.reader.LineReader;
 import org.jline.utils.AttributedString;
+import org.jline.utils.AttributedStringBuilder;
+import org.jline.utils.AttributedStyle;
 
 /**
  * @author Chakib Daii
@@ -28,5 +31,36 @@ public abstract class BaseHighlighter implements Highlighter {
   @Override
   public void setErrorIndex(int errorIndex) {
     originalHighlighter.setErrorIndex(errorIndex);
+  }
+
+
+  public static AttributedString merge(AttributedString base, AttributedString overlay) {
+    if (Objects.isNull(base)) return overlay;
+
+    int length = Math.min(base.length(), overlay.length());
+    AttributedStringBuilder builder = new AttributedStringBuilder();
+
+    for (int i = 0; i < length; i++) {
+      char c = base.charAt(i);
+      AttributedStyle baseStyle = base.styleAt(i);
+      AttributedStyle overlayStyle = overlay.styleAt(i);
+
+      AttributedStyle style =
+              new AttributedStyle(
+                      baseStyle.getStyle() | overlayStyle.getStyle(),
+                      baseStyle.getMask() | overlayStyle.getMask());
+
+      builder.append(String.valueOf(c), style);
+    }
+
+    // Handle any extra content in base
+    if (base.length() > length) {
+      builder.append(base.subSequence(length, base.length()));
+    }
+    if (overlay.length() > length) {
+      builder.append(overlay.subSequence(length, overlay.length()));
+    }
+
+    return builder.toAttributedString();
   }
 }
