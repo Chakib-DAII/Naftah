e4b1e85d16a051c9725d7d233bf05b6f02c88df7
chakib <chakibdaii1995@gmail.com>
2025-07-21 16:46:41 +0200

    Naftah - minor changes

--- src/main/java/org/daiitech/naftah/errors/ExceptionLocalizer.java
+++ src/main/java/org/daiitech/naftah/errors/ExceptionLocalizer.java
@@ -1,19 +1,18 @@
 package org.daiitech.naftah.errors;
 
 import static org.daiitech.naftah.errors.ExceptionUtils.getMostSpecificCause;
+import static org.daiitech.naftah.utils.arabic.ArabicUtils.ARABIC;
 
-import java.util.Locale;
 import java.util.ResourceBundle;
 
 public final class ExceptionLocalizer {
-  private static final Locale ARABIC = new Locale("ar");
-  private static final ResourceBundle bundle = ResourceBundle.getBundle("exceptions", ARABIC);
+  private static final ResourceBundle BUNDLE = ResourceBundle.getBundle("exceptions", ARABIC);
 
   public static String localizeException(Throwable e) {
     e = getMostSpecificCause(e);
     String key = e.getClass().getSimpleName();
     return String.format(
-        bundle.containsKey(key) ? bundle.getString(key) : bundle.getString("default"),
+        BUNDLE.containsKey(key) ? BUNDLE.getString(key) : BUNDLE.getString("default"),
         e.getLocalizedMessage());
   }
 }
