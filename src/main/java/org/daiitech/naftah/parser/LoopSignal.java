9736f73ca847599e5ad73e8ab10c98e910f8eb69
chakib <chakibdaii1995@gmail.com>
2025-07-27 00:08:04 +0200

    Naftah - adding for loop support - LoopSignal to manage break, continue and return logic

---
+++ src/main/java/org/daiitech/naftah/parser/LoopSignal.java
@@ -0,0 +1,29 @@
+package org.daiitech.naftah.parser;
+
+import org.daiitech.naftah.errors.NaftahBugError;
+
+public enum LoopSignal {
+    NONE,
+    CONTINUE,
+    BREAK,
+    RETURN;
+
+    public record LoopSignalDetails(LoopSignal signal, String sourceLabel, String targetLabel, Object result) {
+        public LoopSignalDetails {
+            if (signal == null) {
+                throw new NaftahBugError("LoopSignal لا يمكن أن يكون null.");
+            }
+        }
+
+        public static LoopSignalDetails of(LoopSignal signal, String sourceLabel, String targetLabel, Object result) {
+            return new LoopSignalDetails(signal, sourceLabel, targetLabel, result);
+        }
+
+        public static LoopSignalDetails of(LoopSignal signal, String sourceLabel, String targetLabel) {
+            return new LoopSignalDetails(signal, sourceLabel, targetLabel, null);
+        }
+        public static LoopSignalDetails of(LoopSignal signal, Object result) {
+            return new LoopSignalDetails(signal, null, null, result);
+        }
+    }
+}
