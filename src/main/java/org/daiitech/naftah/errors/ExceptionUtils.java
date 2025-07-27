e8cc767014daec68893b2b3086036e29b7946496
chakib <chakibdaii1995@gmail.com>
2025-07-27 12:16:21 +0200

    Naftah - updated loops logic

--- src/main/java/org/daiitech/naftah/errors/ExceptionUtils.java
+++ src/main/java/org/daiitech/naftah/errors/ExceptionUtils.java
@@ -1,5 +1,9 @@
 package org.daiitech.naftah.errors;
 
+import org.antlr.v4.runtime.Parser;
+
+import static org.daiitech.naftah.parser.NaftahParserHelper.getFormattedTokenSymbols;
+
 public class ExceptionUtils {
   public static Throwable getRootCause(Throwable original) {
     if (original == null) {
@@ -21,4 +25,16 @@ public class ExceptionUtils {
     Throwable rootCause = getRootCause(original);
     return rootCause != null ? rootCause : original;
   }
+
+  public static NaftahBugError newNaftahBugInvalidLoopLabelError(String label, Parser parser) {
+    return new NaftahBugError(
+            String.format(
+                    "لا يمكن استخدام تسمية الحلقة نفسها '%s' في جملة '%s'.",
+                    label,
+                    getFormattedTokenSymbols(
+                            parser.getVocabulary(),
+                            org.daiitech.naftah.parser.NaftahLexer.BREAK,
+                            false)));
+  }
+
 }
