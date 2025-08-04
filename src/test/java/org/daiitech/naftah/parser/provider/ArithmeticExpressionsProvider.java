3b47b6e68e42a18d304b4acc4d1b758ecad90d4a
chakib <chakibdaii1995@gmail.com>
2025-08-05 00:57:29 +0200

    Naftah - updated test providers

--- src/test/java/org/daiitech/naftah/parser/provider/ArithmeticExpressionsProvider.java
+++ src/test/java/org/daiitech/naftah/parser/provider/ArithmeticExpressionsProvider.java
@@ -39,21 +39,21 @@ public class ArithmeticExpressionsProvider implements ArgumentsProvider {
         Arguments.of(true, "2 باقي ٢", 0, null),
         Arguments.of(true, "٢ % 2", 0, null),
         Arguments.of(true, "٢ باقي 2", 0, null),
-        //        Arguments.of(true,"++2", 2, null),
+        Arguments.of(true, "++2", 3, null),
         Arguments.of(true, "2 زد", 2, null),
-        //        Arguments.of(true,"++٢", 2, null),
+        Arguments.of(true, "++٢", 3, null),
         Arguments.of(true, "٢ زد", 2, null),
-        Arguments.of(true, "2++", 2, null), // TODO: check
+        Arguments.of(true, "2++", 2, null),
         Arguments.of(true, "زد 2", 3, null),
-        Arguments.of(true, "٢++", 2, null), // TODO: check
+        Arguments.of(true, "٢++", 2, null),
         Arguments.of(true, "زد ٢", 3, null),
-        //        Arguments.of(true,"--2", 2, null), // TODO: check
+        Arguments.of(true, "--2", 1, null),
         Arguments.of(true, "2 نقص", 2, null),
-        //        Arguments.of(true,"--٢", 2, null), // TODO: check
+        Arguments.of(true, "--٢", 1, null),
         Arguments.of(true, "٢ نقص", 2, null),
         Arguments.of(true, "2--", 2, null),
-        Arguments.of(true, "نقص 2", 1, null), // TODO: check
-        Arguments.of(true, "٢--", 2, null), // TODO: check
+        Arguments.of(true, "نقص 2", 1, null),
+        Arguments.of(true, "٢--", 2, null),
         Arguments.of(true, "نقص ٢", 1, null));
   }
 }
