d4cfac3cfa747280bb657ffbace62395ea7992a3
chakib <chakibdaii1995@gmail.com>
2025-08-06 18:32:41 +0200

    Naftah - tests - block

---
+++ src/test/java/org/daiitech/naftah/parser/provider/BlockProvider.java
@@ -0,0 +1,17 @@
+package org.daiitech.naftah.parser.provider;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+
+public class BlockProvider implements ArgumentsProvider {
+  @Override
+  public Stream<? extends Arguments> provideArguments(ExtensionContext context) throws Exception {
+    return Stream.of(
+        Arguments.of(true, "{ارجع 2}", 2, null),
+        Arguments.of(true, "{أرجع 2}", 2, null),
+        Arguments.of(true, "{اعد 2}", 2, null),
+        Arguments.of(true, "{أعد 2}", 2, null));
+  }
+}
