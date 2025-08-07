d6bb2d302269388a714eeea70b8448add75e8909
chakib <chakibdaii1995@gmail.com>
2025-08-08 00:34:19 +0200

    Naftah - added package info

---
+++ src/main/java/org/daiitech/naftah/builtin/utils/op/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This is the base package of Naftah programming language operations utilities.
+ *
+ * @author Chakib Daii
+ */
+package org.daiitech.naftah.builtin.utils.op;
---
+++ src/main/java/org/daiitech/naftah/utils/repl/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This is the base package of Naftah programming language REPL (Read-Eval-Print Loop) utilities.
+ *
+ * @author Chakib Daii
+ */
+package org.daiitech.naftah.utils.repl;
