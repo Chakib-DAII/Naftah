08fe4eac0cfc2221777ba3f4938dbf04748244d7
chakib <chakibdaii1995@gmail.com>
2025-08-15 19:18:17 +0200

    Naftah - updated NumberUtils - updated tests

--- src/test/java/org/daiitech/naftah/TestUtils.java
+++ src/test/java/org/daiitech/naftah/TestUtils.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.daiitech.naftah.builtin.lang.DeclaredVariable;
 import org.daiitech.naftah.builtin.lang.DynamicNumber;
 import org.daiitech.naftah.builtin.utils.NumberUtils;
+import org.daiitech.naftah.errors.ExceptionUtils;
 import org.daiitech.naftah.errors.NaftahBugError;
 import org.daiitech.naftah.parser.NaftahErrorListener;
 import org.junit.jupiter.api.Assertions;
@@ -14,48 +15,50 @@ import static org.daiitech.naftah.parser.NaftahParserHelper.getCharStream;
 import static org.daiitech.naftah.parser.NaftahParserHelper.prepareRun;
 
 public final class TestUtils {
-    public static Object runScript(String script) throws Exception {
-        var input = getCharStream(false, script);
+	public static Object runScript(String script) throws Exception {
+		var input = getCharStream(false, script);
 
-        var parser = prepareRun(input, NaftahErrorListener.INSTANCE);
+		var parser = prepareRun(input, NaftahErrorListener.INSTANCE);
 
-        return doRun(parser);
-    }
+		return doRun(parser);
+	}
 
-    public static void assertEquals(Object result, Object expectedValue) {
-        if (result instanceof Map<?, ?> map) {
-            var expectedValueMap = (Map<?, ?>) expectedValue;
-            map.forEach((key, value) -> {
-                var expectedValueFromMap = expectedValueMap.get(key);
-                if (value instanceof DeclaredVariable declaredVariable) {
-                    doAssertEquals(expectedValueFromMap, declaredVariable.getValue());
-                }
-                else {
-                    doAssertEquals(expectedValueFromMap, value);
-                }
-            });
-        }
-        else if (result instanceof DeclaredVariable declaredVariable) {
-            doAssertEquals(expectedValue, declaredVariable.getValue());
-        }
-        else {
-            doAssertEquals(result, expectedValue);
-        }
-    }
+	public static void assertEquals(Object result, Object expectedValue) {
+		if (result instanceof Map<?, ?> map) {
+			var expectedValueMap = (Map<?, ?>) expectedValue;
+			map.forEach((key, value) -> {
+				var expectedValueFromMap = expectedValueMap.get(key);
+				if (value instanceof DeclaredVariable declaredVariable) {
+					doAssertEquals(expectedValueFromMap, declaredVariable.getValue());
+				}
+				else {
+					doAssertEquals(expectedValueFromMap, value);
+				}
+			});
+		}
+		else if (result instanceof DeclaredVariable declaredVariable) {
+			doAssertEquals(expectedValue, declaredVariable.getValue());
+		}
+		else {
+			doAssertEquals(result, expectedValue);
+		}
+	}
 
-    public static void doAssertEquals(Object result, Object expectedValue) {
-        if (result instanceof Number) {
-            System.out.println(DynamicNumber.of(result) + "-" + DynamicNumber.of(expectedValue));
-            Assertions.assertTrue(NumberUtils.equals(expectedValue, result));
-        }
-        else {
-            Assertions.assertEquals(expectedValue, result);
-        }
-    }
+	public static void doAssertEquals(Object result, Object expectedValue) {
+		if (result instanceof Number) {
+			System.out.println(DynamicNumber.of(result) + "\n" + DynamicNumber.of(expectedValue));
+			Assertions.assertTrue(NumberUtils.equals(expectedValue, result));
+		}
+		else {
+			Assertions.assertEquals(expectedValue, result);
+		}
+	}
 
-    public static void doAssertBugEquals(String script, NaftahBugError expectedNaftahBugError) {
-        NaftahBugError naftahBugError = Assertions.assertThrows(NaftahBugError.class, () -> runScript(script));
-        Assertions.assertEquals(expectedNaftahBugError.getCause(), naftahBugError.getCause());
-        Assertions.assertEquals(expectedNaftahBugError.getBugText(), naftahBugError.getBugText());
-    }
+	public static void doAssertBugEquals(String script, NaftahBugError expectedNaftahBugError) {
+		NaftahBugError naftahBugError = Assertions.assertThrows(NaftahBugError.class, () -> runScript(script));
+		var expected = ExceptionUtils.getMostSpecificCause(expectedNaftahBugError);
+		var actual = ExceptionUtils.getMostSpecificCause(naftahBugError);
+		Assertions.assertEquals(expected.getClass(), actual.getClass());
+		Assertions.assertEquals(expected.getMessage(), actual.getMessage());
+	}
 }
