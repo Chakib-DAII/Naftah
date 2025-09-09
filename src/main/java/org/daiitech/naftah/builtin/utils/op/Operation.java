d01e0bd1408a14be802092e7048d4812343b50eb
chakib <chakibdaii1995@gmail.com>
2025-09-10 01:40:57 +0200

    Naftah - expression precedence support - Operation changes

--- src/main/java/org/daiitech/naftah/builtin/utils/op/Operation.java
+++ src/main/java/org/daiitech/naftah/builtin/utils/op/Operation.java
@@ -1,5 +1,7 @@
 package org.daiitech.naftah.builtin.utils.op;
 
+import org.daiitech.naftah.errors.NaftahBugError;
+
 /**
  * Represents a general operation in the Naftah language.
  * <p>
@@ -10,4 +12,22 @@ package org.daiitech.naftah.builtin.utils.op;
  * @author Chakib Daii
  */
 public sealed interface Operation permits UnaryOperation, BinaryOperation {
+
+	/**
+	 * Creates a {@link NaftahBugError} indicating that no enum constant was found
+	 * for the given operator symbol in the specified {@code Operation} enum class.
+	 *
+	 * @param operation the enum class where the operator was expected
+	 * @param op        the operator symbol (e.g., "+", "AND", etc.)
+	 * @return a {@code NaftahBugError} with an Arabic error message
+	 */
+	static NaftahBugError newNaftahBugNoEnumValueError(Class<? extends Operation> operation, String op) {
+		return new NaftahBugError(String
+										  .format(
+												  "لا يوجد ثابت في التعداد %s بالرمز '%s'.",
+												  operation.getName(),
+												  op
+										  ));
+	}
+
 }
