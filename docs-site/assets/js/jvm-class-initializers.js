f3f0a11d501e3be2b415dbb2c0ae5da50c6d140e
chakib <chakibdaii1995@gmail.com>
2025-11-15 18:25:25 +0100

    Naftah - docs site improvements

--- docs-site/assets/js/jvm-class-initializers.js
+++ docs-site/assets/js/jvm-class-initializers.js
@@ -1,6 +1,28 @@
+// Scroll to the table top after each draw (paging/filtering)
+function scrollToJvmClassInitializersTable() {
+	var $table = $('#jvm-class-initializers-table');
+
+	// Get the table's top relative to the viewport
+	var tableTop = $table[0].getBoundingClientRect().top + window.pageYOffset;
+
+	// Consider the fixed site header height
+	var siteHeaderHeight = $('.site-header').outerHeight() || 0;
+
+	// DataTables FixedHeader is active, subtract its height as well
+	var fixedHeaderHeight = 0;
+	if ($.fn.DataTable.FixedHeader) {
+		fixedHeaderHeight = $('.fixedHeader-floating').outerHeight() || 0;
+	}
+
+	// Scroll to the correct position
+	$('html, body').animate({
+		scrollTop: tableTop - siteHeaderHeight - fixedHeaderHeight - 100 // extra padding
+	}, 300);
+}
+
 $(document).ready(function() {
 	// JVM Class Initializers
-	$('#jvm-class-initializers-table').DataTable({
+	var table = $('#jvm-class-initializers-table').DataTable({
 		ajax: {
 			url: '/assets/data/jvm-class-initializers.json',
 			dataSrc: ''
@@ -20,4 +42,30 @@ $(document).ready(function() {
 		],
 		language: { url: "https://cdn.datatables.net/plug-ins/1.13.6/i18n/ar.json" }
 	});
+
+	// Track user interactions: pagination, length, search input
+	$(document).on(
+		"input",
+		"#jvm-class-initializers-table_filter > label > input[type=search]",
+		scrollToJvmClassInitializersTable);
+
+    $(document).on(
+        "change",
+        "#jvm-class-initializers-table_length > label > select",
+    	scrollToJvmClassInitializersTable);
+
+	$(document).on(
+		"click",
+		"#jvm-class-initializers-table_paginate > span > a.paginate_button:not(.current)",
+		scrollToJvmClassInitializersTable);
+
+	$(document).on(
+		"click",
+		"#jvm-class-initializers-table_previous",
+		scrollToJvmClassInitializersTable);
+
+	$(document).on(
+		"click",
+		"#jvm-class-initializers-table_next",
+		scrollToJvmClassInitializersTable);
 });
--- docs-site/assets/js/jvm-functions.js
+++ docs-site/assets/js/jvm-functions.js
@@ -1,6 +1,28 @@
+// Scroll to the table top after each draw (paging/filtering)
+function scrollToJvmFunctionsTable() {
+	var $table = $('#jvm-functions-table');
+
+	// Get the table's top relative to the viewport
+	var tableTop = $table[0].getBoundingClientRect().top + window.pageYOffset;
+
+	// Consider the fixed site header height
+	var siteHeaderHeight = $('.site-header').outerHeight() || 0;
+
+	// DataTables FixedHeader is active, subtract its height as well
+	var fixedHeaderHeight = 0;
+	if ($.fn.DataTable.FixedHeader) {
+		fixedHeaderHeight = $('.fixedHeader-floating').outerHeight() || 0;
+	}
+
+	// Scroll to the correct position
+	$('html, body').animate({
+		scrollTop: tableTop - siteHeaderHeight - fixedHeaderHeight - 100 // extra padding
+	}, 300);
+}
+
 $(document).ready(function() {
     // JVM Functions
-    $('#jvm-functions-table').DataTable({
+     var table = $('#jvm-functions-table').DataTable({
         ajax: {
             url: '/assets/data/jvm-functions.json',
             dataSrc: ''
@@ -22,4 +44,30 @@ $(document).ready(function() {
         ],
         language: { url: "https://cdn.datatables.net/plug-ins/1.13.6/i18n/ar.json" }
     });
+
+	// Track user interactions: pagination, length, search input
+	$(document).on(
+		"input",
+		"#jvm-functions-table_filter > label > input[type=search]",
+		scrollToJvmFunctionsTable);
+
+    $(document).on(
+        "change",
+        "#jvm-functions-table_length > label > select",
+    	scrollToJvmFunctionsTable);
+
+	$(document).on(
+		"click",
+		"#jvm-functions-table_paginate > span > a.paginate_button:not(.current)",
+		scrollToJvmFunctionsTable);
+
+	$(document).on(
+		"click",
+		"#jvm-functions-table_previous",
+		scrollToJvmFunctionsTable);
+
+	$(document).on(
+		"click",
+		"#jvm-functions-table_next",
+		scrollToJvmFunctionsTable);
 });
