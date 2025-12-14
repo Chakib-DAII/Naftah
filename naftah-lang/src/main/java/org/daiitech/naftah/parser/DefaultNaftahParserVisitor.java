74e2d8c68dc76d7494a3e0ef516c568b589afcd4
chakib <chakibdaii1995@gmail.com>
2025-12-15 00:40:02 +0100

    Naftah - complex types support - builtin type and instance of support - parser visitor

--- naftah-lang/src/main/java/org/daiitech/naftah/parser/DefaultNaftahParserVisitor.java
+++ naftah-lang/src/main/java/org/daiitech/naftah/parser/DefaultNaftahParserVisitor.java
@@ -3995,6 +3995,22 @@ public class DefaultNaftahParserVisitor extends org.daiitech.naftah.parser.Nafta
 		);
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override public JavaType visitTypeExpression(org.daiitech.naftah.parser.NaftahParser.TypeExpressionContext ctx) {
+		return visitContext(
+				this,
+				"visitTypeExpression",
+				getCurrentContext(),
+				ctx,
+				(   defaultNaftahParserVisitor,
+					currentContext,
+					typeExpressionContext) -> defaultNaftahParserVisitor
+						.visit(typeExpressionContext.type()),
+				JavaType.class
+		);
+	}
 
 	/**
 	 * {@inheritDoc}
