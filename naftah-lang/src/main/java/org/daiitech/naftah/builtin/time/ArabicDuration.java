e95bac47fdc342c000157cafe06040c2d65571d7
chakib <chakibdaii1995@gmail.com>
2025-12-26 17:32:39 +0100

    Naftah - date time support - updated arabic duration

--- naftah-lang/src/main/java/org/daiitech/naftah/builtin/time/ArabicDuration.java
+++ naftah-lang/src/main/java/org/daiitech/naftah/builtin/time/ArabicDuration.java
@@ -28,16 +28,50 @@ public record ArabicDuration(
 ) implements ArabicTemporalAmount {
 
 	/**
-	 * Creates a new {@code ArabicDuration} instance.
+	 * Creates a new {@link ArabicDuration} instance using the provided
+	 * Arabic duration definition and the underlying {@link Duration}.
 	 *
-	 * @param durationDefinition the definition of the duration in Arabic text
-	 * @param temporalAmount     the actual duration value
-	 * @return a new {@code ArabicDuration} instance
+	 * @param durationDefinition the Arabic textual definition describing the duration
+	 *                           (hours, minutes, seconds, etc.)
+	 * @param temporalAmount     the underlying {@link Duration} representing the actual
+	 *                           time-based amount
+	 * @return a new {@link ArabicDuration} instance
 	 */
 	public static ArabicDuration of(
 									DurationDefinition durationDefinition,
 									Duration temporalAmount) {
 		return new ArabicDuration(durationDefinition, temporalAmount);
+	}
+
+	/**
+	 * Creates a new {@link ArabicDuration} instance from a {@link Duration}.
+	 * <p>
+	 * The duration is decomposed into its time-based components
+	 * (hours, minutes, seconds, milliseconds, and nanoseconds) to
+	 * build the corresponding Arabic textual representation.
+	 * </p>
+	 *
+	 * @param duration the {@link Duration} to convert into an {@link ArabicDuration}
+	 * @return a new {@link ArabicDuration} instance representing the given duration
+	 */
+	public static ArabicDuration of(Duration duration) {
+		long totalSeconds = duration.getSeconds();
+		int nanos = duration.getNano();
+
+		int hours = (int) (totalSeconds / 3600);
+		int minutes = (int) ((totalSeconds % 3600) / 60);
+		int seconds = (int) (totalSeconds % 60);
+		int millis = nanos / 1_000_000;
+		int remainingNanos = nanos % 1_000_000;
+
+		return of(DurationDefinition
+				.of(
+					hours,
+					minutes,
+					seconds,
+					millis,
+					remainingNanos
+				), duration);
 	}
 
 	/**
@@ -79,22 +113,27 @@ public record ArabicDuration(
 			String nanoText
 	) {
 		/**
-		 * Creates a new {@code DurationDefinition} instance, filling missing text labels with default Arabic terms.
+		 * Creates a new {@link DurationDefinition} instance.
+		 * <p>
+		 * Any missing Arabic text labels are automatically filled using the
+		 * default Arabic time unit terms defined in
+		 * {@link org.daiitech.naftah.utils.time.Constants}.
+		 * </p>
 		 *
 		 * @param hours      the number of hours
-		 * @param hourText   the Arabic word for hours (default is
-		 *                   * {@link org.daiitech.naftah.utils.time.Constants#HOUR})
+		 * @param hourText   the Arabic text used for the hour unit; if {@code null},
+		 *                   {@link org.daiitech.naftah.utils.time.Constants#HOUR} is used
 		 * @param minutes    the number of minutes
-		 * @param minuteText the Arabic word for minutes (default is
-		 *                   * {@link org.daiitech.naftah.utils.time.Constants#MINUTE})
+		 * @param minuteText the Arabic text used for the minute unit; if {@code null},
+		 *                   {@link org.daiitech.naftah.utils.time.Constants#MINUTE} is used
 		 * @param seconds    the number of seconds
-		 * @param millis     the number of milliseconds (used as decimal part for seconds)
-		 * @param secondText the Arabic word for seconds (default is
-		 *                   * {@link org.daiitech.naftah.utils.time.Constants#SECOND})
+		 * @param millis     the number of milliseconds (used as the decimal fraction of seconds)
+		 * @param secondText the Arabic text used for the second unit; if {@code null},
+		 *                   {@link org.daiitech.naftah.utils.time.Constants#SECOND} is used
 		 * @param nanos      the number of nanoseconds
-		 * @param nanoText   the Arabic word for nanoseconds (default is
-		 *                   * {@link org.daiitech.naftah.utils.time.Constants#NANOSECOND})
-		 * @return a new {@code DurationDefinition} instance
+		 * @param nanoText   the Arabic text used for the nanosecond unit; if {@code null},
+		 *                   {@link org.daiitech.naftah.utils.time.Constants#NANOSECOND} is used
+		 * @return a new {@link DurationDefinition} instance
 		 */
 		public static DurationDefinition of(
 											int hours,
@@ -115,6 +154,34 @@ public record ArabicDuration(
 											Objects.requireNonNullElse(secondText, SECOND),
 											nanos,
 											Objects.requireNonNullElse(nanoText, NANOSECOND));
+		}
+
+		/**
+		 * Creates a new {@link DurationDefinition} instance using the default
+		 * Arabic time unit labels.
+		 *
+		 * @param hours   the number of hours
+		 * @param minutes the number of minutes
+		 * @param seconds the number of seconds
+		 * @param millis  the number of milliseconds (used as the decimal fraction of seconds)
+		 * @param nanos   the number of nanoseconds
+		 * @return a new {@link DurationDefinition} instance
+		 */
+		public static DurationDefinition of(
+											int hours,
+											int minutes,
+											int seconds,
+											int millis,
+											int nanos) {
+			return new DurationDefinition(  hours,
+											HOUR,
+											minutes,
+											MINUTE,
+											seconds,
+											millis,
+											SECOND,
+											nanos,
+											NANOSECOND);
 		}
 
 		/**
